a1c0fbd74fe37f517974cdf15fd9d9d4d284c88b
Valeriia-Ro <vkolchak17@gmail.com>
2021-04-16 19:36:43 +0300

    fixing

--- task-1/index.js
+++ task-1/index.js
@@ -1,10 +1,8 @@
 console.log('message');
 
-const btnStart = document.querySelector('.startClass');
-const btnStop = document.querySelector('.stopClass');
 const bodyColor = document.querySelector('body');
-// const btnStart = document.querySelector('[data-action="start"]');
-// const btnStop = document.querySelector('[data-action="stop"]');
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
 console.log(btnStart);
 
 btnStart.addEventListener('click', onBtnStartClick);
@@ -26,17 +24,13 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// function changeColor () {
-//     onBtnStartClick();
-//     onBtnStopClick();
-// }
 
 function onBtnStartClick (evt) {
 
-    timeoutId = setTimeout(() => {
+    timeId =  setTimeout(() => {
         bodyColor.style.background = colors[index];
-        index = randomIntegerFromInterval();     
-    },CHANGE_COLOR_DELAY)
+        index = randomIntegerFromInterval(0, colors.length-1);
+    },CHANGE_COLOR_DELAY);
 
     if (evt) {
         btnStart.setAttribute('disabled', true)
@@ -47,5 +41,5 @@ function onBtnStartClick (evt) {
 }
 
 function onBtnStopClick () {
-   clearTimeout(timeoutId);
+   clearTimeout(timeId);
 }
